0e189bb821e588769dea3194515beb16829f52c1
ana-balica <ana.balica@gmail.com>
2013-12-11 23:39:34 +0100

    add documentation and cleanup the gdp triplifier

--- download/triplifier/gdp.py
+++ download/triplifier/gdp.py
@@ -7,11 +7,20 @@ from triplifier import env
 
 
 class GDPTriplifier(object):
+    """ Custom triplifier class - converts from csv to ttl (turtle format)
+    """
     def __init__(self, source_file_path):
+        """ Class initializer
+
+        @param source_file_path: path to the csv file to be converted
+        """
         self.source_file_path = source_file_path
-        self.data = None
 
     def triplify(self):
+        """ Convert csv data to turtle format
+
+        @return: dict of lists of the format {country_name: [{year: gdp_value}, ]}
+        """
         countries = {}
         years = [2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012, 2013]
         with open(self.source_file_path, 'r') as csvfile:
@@ -26,21 +35,25 @@ class GDPTriplifier(object):
                         if gdp_value:
                             gdp_data.extend([{year: row[i+offset]}])
                     countries[country_name] = gdp_data
-        self.data = countries
+        return countries
 
-    def render_template(self):
-        if self.data is None:
+    def render_template(self, data):
+        """ Render the template to output a file in ttl format
+
+        @param data: data structure containing infomation about countries and gdp values
+        @rtype: string
+        @return: turtle formated data
+        """
+        if data is None:
             raise TypeError("First triplify the data, then try to render the rdf/turtle file")
-        global env
         template = env.get_template('gdp_template.ttl')
-        return template.render(countries=self.data)
-        
+        return template.render(countries=data)
 
 
 if __name__ == "__main__":
     pwd = os.getcwd()
     gdp = GDPTriplifier(os.path.join(pwd, 'cache/gdp.csv'))
-    gdp.triplify()
-    ttl_data = gdp.render_template()
+    data = gdp.triplify()
+    ttl_data = gdp.render_template(data)
     with open(os.path.join(pwd, 'output/gdp.ttl'), "w") as f:
         f.write(ttl_data)
