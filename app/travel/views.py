7ed47eef9293da6055ec5f03d586606b4b078a38
ana-balica <ana.balica@gmail.com>
2013-12-18 21:17:33 +0100

    index view (GET and POST)

--- app/travel/views.py
+++ app/travel/views.py
@@ -1,5 +1,17 @@
+from flask import render_template, request, flash, redirect, url_for, jsonify
+
 from travel import app
+from forms import CountryForm
+
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-	return "Hello"
+    form = CountryForm(request.form)
+    if request.method == 'POST' and form.validate():
+        country = form.country.data
+        form = CountryForm()
+    return render_template('index.html', form=form)
+
+def get_country_info():
+    pass
+
